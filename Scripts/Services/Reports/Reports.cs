b75d0d2b7862b483d2c0384ec019c08072ba9db0
James Wiegand <jawiegand@gmail.com>
2018-05-11 17:48:28 -0500

    Fix minor error in skills calculation.

--- Scripts/Services/Reports/Reports.cs
+++ Scripts/Services/Reports/Reports.cs
@@ -156,7 +156,7 @@ namespace Server.Engines.Reports
             {
                 // TODO: Re-implement this, see prestige levels
                 //if (mob.SkillsTotal >= 1500 && mob.SkillsTotal <= (Config.Get("PlayerCaps.TotalSkillCap", 7000) + 200) && mob is PlayerMobile)
-                if (mob.SkillsTotal >= 1500 && mob.SkillsTotal <= 7400 && mob is PlayerMobile)
+                if (mob.SkillsTotal >= 1500 && mob.SkillsTotal <= 7200 && mob is PlayerMobile)
                 {
                     Skills skills = mob.Skills;
 
